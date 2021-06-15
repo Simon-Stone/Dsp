a07e05ba81c1ac5cc34f6286f4916c35653bb418
Simon Stone <sipreuss@gmail.com>
2021-06-15 16:00:07 +0200

    Added some Linux-specific CMake code and moved header files to a separate include folder

--- scripts/benchmark_results.py
+++ scripts/benchmark_results.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from scipy import stats
 
 if __name__ == "__main__":
-    res = pd.read_csv("dsp_benchmark.csv", delimiter="\t")
+    res = pd.read_csv("../test/dsp_benchmark.csv", delimiter="\t")
     sns.boxplot(x=res['Task'], y=res['Execution time [us]'], hue=res['Implementation'])
     
     
